33bef815848d0fda62a6f046b48156bafe339c6a
Tomasz Załupski <tzalupski@gmail.com>
2018-11-04 23:02:59 +0100

    add sign up features specs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def after_sign_up_path_for(resource_or_scope)
+    root_path
+  end
+
+
+
   def sign_up_params
     params.require(:user).permit( :name, 
                                   :email, 
