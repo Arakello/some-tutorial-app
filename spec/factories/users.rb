bafcfe3f6a728e9587c008350215a2c889502f6b
Tomasz Załupski <tzalupski@gmail.com>
2018-11-04 19:51:24 +0100

    add a users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "test#{n}@test.com" }
+    sequence(:name) { |n| "test#{n}" }
+    password '123456'
+    password_confirmation '123456'
   end
 end
