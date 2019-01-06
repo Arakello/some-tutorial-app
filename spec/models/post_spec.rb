d7e722e9d7518c0a8340091c20ee1e8c3faf2ff4
Tomasz Załupski <tzalupski@gmail.com>
2019-01-07 00:44:38 +0100

    Add a spec for the Post model's default_scope

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Scopes' do
+    it 'default_scope orders by descending created_at' do
+      first_post = create(:post)
+      second_post = create(:post)
+      expect(Post.all).to eq [second_post, first_post]
+    end
+  end
 end
